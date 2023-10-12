728257f5cd238e20faebb7bd89cf3c1a24bd8836
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-10-11 20:58:56 -0500

    completed turn-based rpg

---
+++ turn-based-rpg/Character.gd
@@ -0,0 +1,80 @@
+extends Node2D
+class_name Character
+
+@export var is_player : bool
+@export var cur_hp : int
+@export var max_hp : int
+
+@export var combat_actions : Array
+@export var opponent : Node
+
+@onready var health_bar : ProgressBar = get_node("HealthBar")
+@onready var health_text : Label = get_node("HealthBar/HealthText")
+
+@export var visual : Texture2D
+@export var flip_visual : bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite.texture = visual
+	$Sprite.flip_h = flip_visual
+	get_node("/root/BattleScene").character_begin_turn.connect(_on_character_begin_turn)
+	health_bar.max_value = max_hp
+	
+
+func take_damage(damage):
+	cur_hp -= damage
+	if cur_hp <= 0:
+		get_node("/root/BattleScene").character_died(self)
+		queue_free()
+	
+	_update_health_bar()
+
+func heal(amount):
+	cur_hp += amount
+	
+	if cur_hp >= max_hp:
+		cur_hp = max_hp
+	
+	_update_health_bar()
+
+func _update_health_bar():
+	print(str("updating health: ", cur_hp))
+	health_bar.value = cur_hp
+	health_text.text = str(cur_hp, ' / ', max_hp)
+
+func _on_character_begin_turn(character):
+	if character == self and is_player == false:
+		_decide_combat_action()
+
+func cast_combat_action(action):
+	if action.damage > 0:
+		opponent.take_damage(action.damage)
+	elif action.heal > 0:
+		heal(action.heal)
+	
+	get_node("/root/BattleScene").end_current_turn()
+
+func _decide_combat_action():
+	var health_percent : float = float(cur_hp) / float(max_hp)
+	
+	for i in combat_actions:
+		var action = i as CombatAction
+		if action.heal > 0:
+			if randf() > health_percent + 0.2:
+				cast_combat_action(action)
+				return
+			else:
+				continue
+		else:
+			cast_combat_action(action)
+			return
+			
+		
+		
+		
+		
+		
+		
+		
+		
---
+++ turn-based-rpg/TurnManager.gd
@@ -0,0 +1,48 @@
+extends Node
+
+@export var player_char : Node
+@export var enemy_char : Node
+
+var cur_char : Character
+@export var next_turn_delay : float = 1.0
+
+var game_over : bool = false
+
+signal character_begin_turn(character)
+signal character_end_turn(character)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await get_tree().create_timer(next_turn_delay).timeout
+	begin_next_turn()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func begin_next_turn():
+	if cur_char == player_char:
+		cur_char = enemy_char
+	elif cur_char == enemy_char:
+		cur_char = player_char
+	else:
+		cur_char = player_char
+	
+	emit_signal("character_begin_turn", cur_char)
+
+func end_current_turn():
+	emit_signal("character_end_turn", cur_char)
+	
+	await get_tree().create_timer(next_turn_delay).timeout
+	
+	if game_over == false:
+		begin_next_turn()
+
+func character_died(character):
+	game_over = true
+	
+	if character.is_player == true:
+		print("Game over!")
+	else:
+		print("You win!")
